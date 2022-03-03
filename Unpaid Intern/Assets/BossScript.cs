1803138d8489fa1165410ad97cc0d86045f9ab0c
smoon09 <98260990+smoon09@users.noreply.github.com>
2022-03-03 15:03:52 +0900

    Created new scene to combine

--- Unpaid Intern/Assets/BossScript.cs
+++ Unpaid Intern/Assets/BossScript.cs
@@ -4,35 +4,45 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
-    public GameObject distractedBossArt;
-    public GameObject focusedBossArt;
-    public GameObject curiousBossArt;
+    // public GameObject distractedBossArt;
+    // public GameObject focusedBossArt;
+    // public GameObject curiousBossArt;
     public static bool isDistracted = true;
     private int rangeMax = 3000;
     private int randomNum;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        focusedBossArt.SetActive(false);
-        curiousBossArt.SetActive(false);
+        // focusedBossArt.SetActive(false);
+        // curiousBossArt.SetActive(false);
+        animator = GetComponentInChildren<Animator>();
+    }
+    
+     void Update() {
         StartCoroutine(changeFocus());
+
     }
 
     IEnumerator changeFocus(){
        while (true){
            if (randomNum == 45){
                 if (isDistracted) {
-                    curiousBossArt.SetActive(true);
+                    // curiousBossArt.SetActive(true);
                     yield return new WaitForSeconds(2);
-                    distractedBossArt.SetActive(false);
-                    curiousBossArt.SetActive(false);
-                    focusedBossArt.SetActive(true);
+                    animator.SetBool("Turn", false);
+                    // distractedBossArt.SetActive(false);
+                    // curiousBossArt.SetActive(false);
+                    // focusedBossArt.SetActive(true);
+                    
+
                     isDistracted = false;
                 } else {
                     yield return new WaitForSeconds(2);
-                    distractedBossArt.SetActive(true);
-                    focusedBossArt.SetActive(false);
+                    // distractedBossArt.SetActive(true);
+                    animator.SetBool("Turn", true);
+                    // focusedBossArt.SetActive(false);
                     isDistracted = true;
                 }
            }
@@ -44,3 +54,29 @@ public class BossScript : MonoBehaviour
         randomNum = Random.Range(0, rangeMax);
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Player : MonoBehaviour
+// {
+//     public Animator animator;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         animator = GetComponentInChildren<Animator>();
+//     }
+
+//     void Update() {
+//         if(Input.GetKey("space")) {
+//             animator.SetBool("Chattering", true);
+//         }
+//         else {
+//             animator.SetBool("Chattering", false);
+//         }
+//     }
+
+//     // Update is called once per frame
+// }
--- Unpaid Intern/Assets/Player.cs
+++ Unpaid Intern/Assets/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     
     // public GameObject PlayerArt;
-    public GameObject SpeechBubbleArt;
+    // public GameObject SpeechBubbleArt;
     public static float playerTimer = 0f;
     public static float incrementValue = .001f;
     public static bool isTalking = false;
@@ -13,7 +13,7 @@ public class Player : MonoBehaviour {
 
     void Start() {
         // PlayerArt.SetActive(true);
-        SpeechBubbleArt.SetActive(false);
+        // SpeechBubbleArt.SetActive(false);
         animator = GetComponentInChildren<Animator>();
     }
 
@@ -21,13 +21,41 @@ public class Player : MonoBehaviour {
         if (Input.GetKey("space") && Timer.gameTime > 0){
             animator.SetBool("Chattering", true);
             isTalking = true;
-            SpeechBubbleArt.SetActive(true);
+            // SpeechBubbleArt.SetActive(true);
             playerTimer += incrementValue;
         } else {
             animator.SetBool("Chattering", false);
             isTalking = false;
-            SpeechBubbleArt.SetActive(false);
+            // SpeechBubbleArt.SetActive(false);
 
         }
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Player : MonoBehaviour
+// {
+//     public Animator animator;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         animator = GetComponentInChildren<Animator>();
+//     }
+
+//     void Update() {
+//         if(Input.GetKey("space")) {
+//             animator.SetBool("Chattering", true);
+//         }
+//         else {
+//             animator.SetBool("Chattering", false);
+//         }
+//     }
+
+//     // Update is called once per frame
+// }
+
+
+
