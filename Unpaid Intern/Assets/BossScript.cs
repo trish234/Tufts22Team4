5511b6e580d57990dd1cb06a3eb08147a1059859
smoon09 <98260990+smoon09@users.noreply.github.com>
2022-03-03 11:08:02 +0900

    none

--- Unpaid Intern/Assets/BossScript.cs
+++ Unpaid Intern/Assets/BossScript.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 
 public class BossScript : MonoBehaviour
 {
-    public GameObject distractedBossArt;
-    public GameObject focusedBossArt;
-    public bool isDistracted = true;
-    public int rangeMax = 900;
-    private int randomNum;
+    // public GameObject distractedBossArt;
+    // public GameObject focusedBossArt;
+    // public bool isDistracted = true;
+    // public int rangeMax = 900;
+    // private int randomNum;
 
     // Start is called before the first frame update
     void Start()
     {
-        focusedBossArt.SetActive(false);
+        // focusedBossArt.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (randomNum == 45){
-            ChangeFocus();
-        }
-        RandomMaker();
+    //     if (randomNum == ){
+    //         ChangeFocus();
+    //     }
+    //     RandomMaker();
     }
     void ChangeFocus(){
-        isDistracted = !isDistracted;
-        if (isDistracted) {
-            focusedBossArt.SetActive(false);
-            distractedBossArt.SetActive(true);
-        } else {
-            distractedBossArt.SetActive(false);
-            focusedBossArt.SetActive(true);
-        }
+        // isDistracted = !isDistracted;
+        // if (isDistracted) {
+        //     focusedBossArt.SetActive(false);
+        //     distractedBossArt.SetActive(true);
+        // } else {
+        //     distractedBossArt.SetActive(false);
+        //     focusedBossArt.SetActive(true);
+        // }
     }
     public void RandomMaker(){
-        randomNum = Random.Range(0, rangeMax);
+        // randomNum = Random.Range(0, rangeMax);
     }
 }
